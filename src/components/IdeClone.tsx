b93e373fa2187a5d2cd5c20c58212c0d44342eb3
Mayank <7silenteyes07@gmail.com>
2024-04-26 12:24:12 +0530

    coments

--- src/components/IdeClone.tsx
+++ src/components/IdeClone.tsx
@@ -4,34 +4,39 @@ import { useResizable } from "react-resizable-layout";
 import { cn } from "../utils/cn";
 
 const Mydrag = (): JSX.Element => {
-  const {
-    isDragging: isTerminalDragging,
-    position: terminalH,
-    splitterProps: terminalDragBarProps
-  } = useResizable({
-    axis: "y",
-    initial: 250,
-    min: 50,
-    reverse: true
-  });
+  // State and props for resizing Component - 1 (axis: x)
   const {
     isDragging: isFileDragging,
     position: fileW,
     splitterProps: fileDragBarProps
   } = useResizable({
     axis: "x",
-    initial: 500,
-    min: 50
+    initial: 500, // Initial width of Component - 1
+    min: 50 // Minimum width of Component - 1
   });
+
+  // State and props for resizing Component - 3 (axis: y)
+  const {
+    isDragging: isTerminalDragging,
+    position: terminalH,
+    splitterProps: terminalDragBarProps
+  } = useResizable({
+    axis: "y",
+    initial: 250, // Initial height of Component - 3
+    min: 50, // Minimum height of Component - 3
+    reverse: true // Reverse resizing direction
+  });
+
+  // State and props for resizing Component - 2 (axis: x)
   const {
     isDragging: isPluginDragging,
     position: pluginW,
     splitterProps: pluginDragBarProps
   } = useResizable({
     axis: "x",
-    initial: 200,
-    min: 50,
-    reverse: true
+    initial: 200, // Initial width of Component - 2
+    min: 50, // Minimum width of Component - 2
+    reverse: true // Reverse resizing direction
   });
 
   return (
@@ -40,44 +45,54 @@ const Mydrag = (): JSX.Element => {
         "flex flex-column h-screen bg-dark font-mono color-white overflow-hidden"
       }
     >
+      {/* Top section containing Component - 1, Component - 2, and their splitters */}
       <div className={"flex grow"}>
+        {/* Component - 1 */}
         {Component1(isFileDragging, fileW)}
+        {/* Splitter for Component - 1 */}
         <SampleSplitter isDragging={isFileDragging} {...fileDragBarProps} />
+        {/* Component - 2 */}
         <div className={"flex grow"}>
           <div className={"grow bg-darker contents"}>Component - 2</div>
         </div>
       </div>
+      {/* Splitter for resizing vertically between Component - 2 and Component - 3 */}
       <SampleSplitter
         dir={"horizontal"}
         isDragging={isTerminalDragging}
         {...terminalDragBarProps}
       />
-      {Component2(isTerminalDragging, terminalH)}
+      {/* Component - 3 */}
+      {Component3(isTerminalDragging, terminalH)}
     </div>
   );
 };
 
-export default Mydrag;
-
-
-function Component2(isTerminalDragging: boolean, terminalH: number) {
-  return <div
-    className={cn(
-      "shrink-0 bg-darker contents",
-      isTerminalDragging && "dragging"
-    )}
-    style={{ height: terminalH }}
-  >
-    Component - 3
-  </div>;
+// Function component for rendering Component - 3
+function Component3(isTerminalDragging: boolean, terminalH: number) {
+  return (
+    <div
+      className={cn(
+        "shrink-0 bg-darker contents",
+        isTerminalDragging && "dragging"
+      )}
+      style={{ height: terminalH }}
+    >
+      Component - 3
+    </div>
+  );
 }
 
+// Function component for rendering Component - 1
 function Component1(isFileDragging: boolean, fileW: number) {
-  return <div
-    className={cn("shrink-0 contents", isFileDragging && "dragging")}
-    style={{ width: fileW }}
-  >
-    Component - 1
-  </div>;
+  return (
+    <div
+      className={cn("shrink-0 contents", isFileDragging && "dragging")}
+      style={{ width: fileW }}
+    >
+      Component - 1
+    </div>
+  );
 }
 
+export default Mydrag;
